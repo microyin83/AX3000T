9d8a2419b71c79293100340508d6711134c0663f
microyin83 <153605636+microyin83@users.noreply.github.com>
2023-12-12 21:10:37 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,3 +14,11 @@
 sed -i 's/192.168.1.1/192.168.32.0/g' package/base-files/files/bin/config_generate
 git clone https://github.com/EOYOHOO/UA2F.git package/UA2F
 git clone https://github.com/EOYOHOO/rkp-ipid.git package/rkp-ipid
+
+
+# themes添加（svn co 命令意思：指定版本如https://github）
+git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom package/luci-theme-infinityfreedom
+git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/luci-theme-opentomcat
+
+# 去除默认主题
+sed -i 's/+luci-theme-bootstrap/ /g' feeds/luci/collections/luci/Makefile
