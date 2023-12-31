79171c9b3404e1998d2a0fbf50728b25157580c0
microyin83 <153605636+microyin83@users.noreply.github.com>
2023-12-31 10:33:19 +0800

    Update diy-part2.sh

--- RAX3000M/diy-part2.sh
+++ RAX3000M/diy-part2.sh
@@ -12,3 +12,16 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.33.1/g' package/base-files/files/bin/config_generate
+
+# themes添加（svn co 命令意思：指定版本如https://github）
+git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom package/luci-theme-infinityfreedom
+# git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/luci-theme-opentomcat
+
+# 去除默认主题
+sed -i 's/+luci-theme-bootstrap/ /g' feeds/luci/collections/luci/Makefile
+
+# mosdns
+# find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
+# find ./ | grep Makefile | grep mosdns | xargs rm -f
+# git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+# git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
